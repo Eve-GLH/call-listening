d588967213e3b9d957c81e4d5878bf7b6c7451b4
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2023-11-23 15:21:05 +0000

    made it work

--- functions.js
+++ functions.js
@@ -1,2 +1,34 @@
 //Call Details Section
-const callDetails = document.querySelector('subtotal__details');
+
+    
+
+const submit = document.getElementById("submit");
+const inputs = document.querySelectorAll('input[data-section="details"]');
+const selectors = document.querySelectorAll('select[data-section="details"]');
+
+submit.addEventListener("click", () => {
+   let inputText;
+   let inputTitle;
+   let inputId;
+   let selectorText;
+   let selectorTitle;
+   let selectorId;
+
+   for (const input of inputs) {
+    if (input.value) {
+        inputText = input.value;
+        inputTitle = input.name;
+        inputId = document.getElementById(`${inputTitle}Output`);
+        inputId.innerHTML = inputText;
+    }
+   }
+
+   for (const selector of selectors) {
+    if (selector.value){
+        selectorText = selector.value;
+        selectorTitle = selector.name;
+        selectorId = document.getElementById(`${selectorTitle}Output`);
+        selectorId.innerHTML = selectorText;
+    }
+   }
+});
